73ba5a23e4bcb966f4ed131e263ac8906fb2224a
OttoRichardson <otto.c.richardson@gmail.com>
2025-11-05 15:31:17 +0000

    Added timestamp and saved

--- ectract.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-import requests
-
-url = 'https://api.tfl.gov.uk/BikePoint/'
-
-response = requests.get(url,timeout=10)
-
-print(response.status_code)
---
+++ extract.py
@@ -0,0 +1,22 @@
+import json
+import time
+from datetime import datetime
+import requests
+
+url = 'https://api.tfl.gov.uk/BikePoint/'
+
+response = requests.get(url,timeout=10)
+
+print(response.status_code)
+
+data = response.json()
+
+extract_timestamp = datetime.now()
+
+for bp in data:
+    bp['extract_timestamp'] = str(extract_timestamp)
+
+filepath = 'data/' + extract_timestamp.strftime('%Y-%m-%dT%H-%M-%S') + '.json'
+
+with open(filepath, 'w') as file:
+    json.dump(data,file)
