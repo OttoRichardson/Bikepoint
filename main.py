d44193fcca31347f63be60b7997b3d383e45a5ac
OttoRichardson <otto.c.richardson@gmail.com>
2025-11-12 12:25:20 +0000

    reqs

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+
 from extract import extract
 from load import load
+
+import os
+import sys
+from dotenv import load_dotenv
+import boto3
+import json
+import time
+from datetime import datetime
+import requests
 
 url = 'https://api.tfl.gov.uk/BikePoint/'
 
